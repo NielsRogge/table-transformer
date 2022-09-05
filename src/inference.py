fe0eea5b45872e853c0d9a3ceecaff6c706a6a96
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-09-05 21:03:32 +0200

    Make argument not required

--- src/inference.py
+++ src/inference.py
@@ -27,7 +27,7 @@ def get_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--data_root_dir',
-                        required=True,
+                        required=False,
                         help="Root data directory for images and labels")
     parser.add_argument('--config_file',
                         required=True,
